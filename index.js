75e3d643708d34243ffcec882df883ff0d394f04
Rohan <deychowdhuryrohan@gmail.com>
2025-06-11 00:14:15 +0530

    home page 2

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const gamemode1btn=document.getElementById('gamemode1');
 const gamemode2btn=document.getElementById('gamemode2');
 
-const names2v2=document.getElementById('names2v2');
+const p3namefield=document.getElementById('p3name');
+const p4namefield=document.getElementById('p4name');
+
+const startbtn=document.getElementById('start');
 
 gamemode1btn.onclick=function(){
-    names2v2.style.visibility='hidden';
+    p3namefield.style.visibility='hidden';
+    p4namefield.style.visibility='hidden';
+    startbtn.style.top="60%";
 }
 
 gamemode2btn.onclick=function(){
-    names2v2.style.visibility='visible';
+    p3namefield.style.visibility='visible';
+    p4namefield.style.visibility='visible';
+    startbtn.style.top="80%";
+}
+
+startbtn.onclick=function(){
+
+
+    window.open('https://kilsw1ch.github.io/Carrom-Counter/carrom.html');
 }
